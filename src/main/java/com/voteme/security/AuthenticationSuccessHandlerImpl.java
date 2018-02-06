fc57d78055f18030b16bb5c95c4df051fc7f2aef
xoxolische <nikitos_ua@ukr.net>
2018-02-06 12:41:53 +0200

    add redirect to requested page after successful login

--- src/main/java/com/voteme/security/AuthenticationSuccessHandlerImpl.java
+++ src/main/java/com/voteme/security/AuthenticationSuccessHandlerImpl.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 
 import com.voteme.model.UserAuth;
@@ -17,7 +20,7 @@ import com.voteme.service.OpinionMarkService;
 import com.voteme.service.VersusMarkService;
 
 @Component
-public class AuthenticationSuccessHandlerImpl implements AuthenticationSuccessHandler {
+public class AuthenticationSuccessHandlerImpl extends SavedRequestAwareAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
 	@Autowired
 	private OpinionMarkService opMarkService;
@@ -28,12 +31,19 @@ public class AuthenticationSuccessHandlerImpl implements AuthenticationSuccessHa
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication auth)
 			throws IOException, ServletException {
+		SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
+		//System.out.println(savedRequest.getRedirectUrl());
+		
 		UserAuth userAuth = (UserAuth) auth.getPrincipal();
 		HttpSession session = request.getSession();
 		session.setAttribute("currentUserId", userAuth.getId());
 		session.setAttribute("currentUserVersusMarks", versusMarkService.getByUser(userAuth.getId()));
 		session.setAttribute("currentUserOpinionMarks", opMarkService.getByUser(userAuth.getId()));
-		response.sendRedirect(request.getContextPath()+"/home");
+		if(savedRequest != null && savedRequest.getRedirectUrl() != null) {
+			response.sendRedirect(savedRequest.getRedirectUrl());
+		}else {			
+			response.sendRedirect(request.getContextPath()+"/home");
+		}
 		// switch (userAuth.getRole().getName()) {
 		// case "ADMIN":
 		// response.sendRedirect("/VoteMe/home");
